1b46110501f6ffbb9ad7de2a4f4973f881fb8c6a
AdamNagao <nagaoadam223@gmail.com>
2017-06-20 15:49:28 -0700

    Completed carousel

--- resources/views/projectspage.blade.php
+++ resources/views/projectspage.blade.php
@@ -11,8 +11,9 @@
     <link rel="icon" href="../../favicon.ico">
 
     <title>Carousel Template for Bootstrap</title>
-
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <style>
 /* GLOBAL STYLES
 -------------------------------------------------- */
@@ -214,54 +215,50 @@ body {
 
     <!-- Carousel
     ================================================== -->
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Example headline.</h1>
-              <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Another example headline.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>One more for good measure.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div><!-- /.carousel -->
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="/myWebsite/resources/views/images/quickenrobot.jpg" alt="quickenrobot">
+        <h1>Quicken Loans Rocket Mortgage Commercial</h1>
+        <p>A Robot Built for a Quicken Loan commercial featuring battlebots!</p>
+    </div>
+
+    <div class="item">
+      <img src="/myWebsite/resources/views/images/wheretoeatIcon.jpg" alt="wheretoeatIcon">
+        <h1>Where to Eat App!</h1>
+        <p>A Web application developed with HTML,CSS,Javascript,PHP,Bootstrap, and Laravel. Reviewers can browse and review restaurants created by Admin users.</p>
+    </div>
+
+    <div class="item">
+      <img src="/myWebsite/resources/views/images/syntaxerror.jpg" alt="syntaxerror">
+        <h1>Syntax Error Robotic's 2nd 60lb Combat Robot</h1>
+        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+           
+
 
 
     <!-- Marketing messaging and featurettes
@@ -273,19 +270,19 @@ body {
       <!-- Three columns of text below the carousel -->
       <div class="row">
         <div class="col-lg-4">
-          <img class="img-circle" src="http://localhost/myWebsite/resources/views/hangmanIcon.png" alt="Generic placeholder image" width="140" height="140">
+          <img class="img-circle" src="/myWebsite/resources/views/images/hangmanIcon.png" alt="Generic placeholder image" width="140" height="140">
           <h2>Hangman Game</h2>
           <p>A Hangman game made with HTML,CSS,Javascript, and JQuery</p>
           <p><a class="btn btn-default" href="hangmangame" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" src="http://localhost/myWebsite/resources/views/jafarsjavaIcon.png" alt="Generic placeholder image" width="140" height="140">
+          <img class="img-circle" src="/myWebsite/resources/views/images/jafarsjavaIcon.png" alt="Generic placeholder image" width="140" height="140">
           <h2>Jafar's Java</h2>
           <p>A Disney Aladdin themed coffee restaurant order system where customers can place orders and view orders. Baristas can view, and complete orders made by customers. This project was developed in HTML,CSS,Javascript, PHP, and mySQL.</p>
           <p><a class="btn btn-default" href="jafarsjava" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" src="http://localhost/myWebsite/resources/views/wheretoeatIcon.jpg" alt="Generic placeholder image" width="140" height="140">
+          <img class="img-circle" src="/myWebsite/resources/views/images/wheretoeatIcon.jpg" alt="Generic placeholder image" width="140" height="140">
           <h2>WhereToEat App</h2>
           <p>A Web application developed with HTML,CSS,Javascript,PHP,Bootstrap, and Laravel. Reviewers can browse and review restaurants created by Admin users.</p>
           <p><a class="btn btn-default" href="wheretoeat" role="button">View details &raquo;</a></p>
@@ -295,21 +292,21 @@ body {
             <!-- Three columns of text below the carousel -->
       <div class="row">
         <div class="col-lg-4">
-          <img class="img-circle" src="http://localhost/myWebsite/resources/views/emailsystemIcon.png" alt="Generic placeholder image" width="140" height="140">
+          <img class="img-circle" src="/myWebsite/resources/views/images/emailsystemIcon.png" alt="Generic placeholder image" width="140" height="140">
           <h2>Email System</h2>
           <p>The frontend of a email system styled after gmail, ulitizing HTML,CSS, and Bootstrap</p>
           <p><a class="btn btn-default" href="emailsystem" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" src="http://localhost/myWebsite/resources/views/pokergameIcon.jpg" alt="Generic placeholder image" width="140" height="140">
+          <img class="img-circle" src="/myWebsite/resources/views/images/pokergameIcon.jpg" alt="Generic placeholder image" width="140" height="140">
           <h2>Poker Game</h2>
           <p>A online poker game for 2-4 players using Laravel, JQuery, Javascript,Node, Socket, MySQL,HTML,CSS. Networking is done using socket.</p>
           <p><a class="btn btn-default" href="pokergame" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" src="" alt="Generic placeholder image" width="140" height="140">
-          <h2>Placeholder</h2>
-          <p>Placeholder</p>
+          <img class="img-circle" src="/myWebsite/resources/views/images/quadcopter.jpg" alt="Generic placeholder image" width="140" height="140">
+          <h2>FPV Racing Quadcopter</h2>
+          <p>A FPV Quadcopter built to go super fast! Calculated top speeds of 100mph</p>
           <p><a class="btn btn-default" href="wheretoeat" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         
